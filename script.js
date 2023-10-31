f3adbf3ca5d5a34ef2be63af2a1b576a1108c72d
uhohjackieson <109990923+uhohjackieson@users.noreply.github.com>
2023-10-31 15:35:52 -0400

    added event listener

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 'use strict';
 
-console.log(document.querySelector('.message'));
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.number').textContent = 15;
+// document.querySelector('.score');
+
+// document.querySelector('.guess').value = 23;
+document.querySelector('.check').addEventListener('click', function () {
+  console.log(document.querySelector('.guess').value);
+});
